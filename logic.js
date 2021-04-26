0b57c9e934617aa003382a2e89c44c344f670a8b
Mates0 <matyas.vanko@educanet.cz>
2021-04-26 10:00:48 +0200

    final

--- logic.js
+++ logic.js
@@ -14,9 +14,35 @@ for (let i = cards2.length - 1; i > 0; i--) {
     cards2[j] = temp
 }
 
-let div = document.createElement("div")
+let outerdiv = document.getElementById("outerdivid")
 for (let i = 0; i < cards2.length; i++) {
+    let div = document.createElement("div")
+    div.id = "pexeso" + i
     div.innerHTML += "<h1>" + cards2[i] + "</h1>"
-    document.body.appendChild(div)
+    div.innerHTML += "<h1 class='firstrow' id='hidden'>" + " " + "</h1>"
+    outerdiv.appendChild(div)
+}
+document.body.appendChild(outerdiv)
+
+for (let i = 0; i < cards2.length; i++) {
+    let test = document.getElementById("hidden")
+    test.id = i
 }
 
+let count = 0
+for (let i = 0; i < cards2.length; i++) {
+    document.getElementById(i).addEventListener("click", function () {
+        document.getElementById(i).style.display = "none"
+        count++
+        console.log(count)
+        if (count === 2) {
+            count = 0
+            setTimeout(timeout, 1000)
+        }
+    })
+    function timeout() {
+        for (let j = 0; j < cards2.length; j++) {
+            document.getElementById(j).style.display = "block"
+        }
+    }
+}
